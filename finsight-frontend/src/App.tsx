0015d484f2508341e09bfadcc5cd5b2d3a0bef9c
meliskara-dev <meliskara-dev@gmail.com>
2026-07-30 21:30:32 +0300

    update file structure and impl frontend project

---
+++ finsight-frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { APP_NAME } from '@/lib/utils'
+
+export default function App() {
+  return (
+    <div className="grid min-h-screen place-items-center">
+      <h1 className="text-4xl font-bold text-emerald-600">{APP_NAME}</h1>
+    </div>
+  )
+}

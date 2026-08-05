d3febef66f7adf97c42e7413b5e8bb6d157ce6b8
Ece Nisa Ugur <ecenisaugu@gmail.com>
2026-08-06 00:13:37 +0300

    feat(stresstest): add error message translations for model inference failure

--- finsight-backend/src/main/java/com/akademi/finsight/stresstest/exception/StressTestErrorType.java
+++ finsight-backend/src/main/java/com/akademi/finsight/stresstest/exception/StressTestErrorType.java
@@ -11,7 +11,8 @@ public enum StressTestErrorType implements BaseErrorType {
     MODEL_NOT_FOUND("error.stresstest.model.not.found", HttpStatus.NOT_FOUND),
     INVALID_MODEL_KEY("error.stresstest.invalid.model.key", HttpStatus.BAD_REQUEST),
     MODEL_INITIALIZATION_ERROR("error.stresstest.model.init.error", HttpStatus.INTERNAL_SERVER_ERROR),
-    MODEL_NOT_AVAILABLE("error.stresstest.model.not.available", HttpStatus.SERVICE_UNAVAILABLE);
+    MODEL_NOT_AVAILABLE("error.stresstest.model.not.available", HttpStatus.SERVICE_UNAVAILABLE),
+    MODEL_INFERENCE_ERROR("error.stresstest.model.inference.failed", HttpStatus.INTERNAL_SERVER_ERROR);
 
     StressTestErrorType(String s, HttpStatus httpStatus) {
     }
---
+++ finsight-backend/src/main/java/com/akademi/finsight/stresstest/service/impl/StressTestSimulationServiceImpl.java
@@ -0,0 +1,4 @@
+package com.akademi.finsight.stresstest.service.impl;
+
+public class StressTestSimulationServiceImpl {
+}
